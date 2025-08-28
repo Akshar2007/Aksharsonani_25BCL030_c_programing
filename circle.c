fa3edc9f49aeb48117406e91164a274ac4e47ab0
Akshar2007 <88934310+Akshar2007@users.noreply.github.com>
2025-08-28 23:14:09 +0530

    Create circle.c

---
+++ circle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float twentytwo = 22, seven = 7, r;
+    printf("Enter Radius: ");
+    scanf("%f", &r);
+    float area = (twentytwo/seven) * r * r;
+    printf("Area = %f\n", area);
+    return 0;
+}
