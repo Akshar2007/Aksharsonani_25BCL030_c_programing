54441f087d3c4ddf5a13df2622d787a4c437f2cc
Akshar2007 <88934310+Akshar2007@users.noreply.github.com>
2025-08-28 23:14:44 +0530

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    float base, height;
+    printf("Enter Base: ");
+    scanf("%f", &base);
+    printf("Enter Height: ");
+    scanf("%f", &height);
+    printf("Area = %f\n", (base * height) / 2);
+    return 0;
+}
+
+
